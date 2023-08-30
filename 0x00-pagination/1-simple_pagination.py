208dd2851e2ea2d1d8d0ff05625bfac8f086ec50
emmanueldavids <emmanueldavids417@gmail.com>
2023-08-30 11:59:58 +0000

    updated files

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
-
-
+"""Simple pagination sample.
+"""
 import csv
-import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,34 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int) and page > 0 and page_size > 0
-
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        if start >= len(self.__dataset):
+        data = self.dataset()
+        if start > len(data):
             return []
-
-        return self.__dataset[start:end]
-
-
-# Test the Server class
-if __name__ == "__main__":
-    server = Server()
-
-    try:
-        should_err = server.get_page(-10, 2)
-    except AssertionError:
-        print("AssertionError raised with negative values")
-
-    try:
-        should_err = server.get_page(0, 0)
-    except AssertionError:
-        print("AssertionError raised with 0")
-
-    try:
-        should_err = server.get_page(2, 'Bob')
-    except AssertionError:
-        print("AssertionError raised when page and/or page_size are not ints")
-
-    print(server.get_page(1, 3))
-    print(server.get_page(3, 2))
-    print(server.get_page(3000, 100))
+        return data[start:end]
--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,62 +1,63 @@
+#!/usr/bin/env python3
+"""Hypermedia pagination sample.
+"""
 import csv
 import math
-from typing import List
+from typing import Dict, List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
 
 class Server:
-    DATA_FILE = "data/Popular_Baby_Names.csv"
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
-        self.load_dataset()
 
-    def load_dataset(self):
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
 
-    def dataset(self) -> List[List]:
-        self.load_dataset()
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int) and page > 0 and page_size > 0
-
-        start, end = self.index_range(page, page_size)
-        if start >= len(self.__dataset):
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
 
-        return self.__dataset[start:end]
-
-    def index_range(self, page: int, page_size: int) -> tuple:
-        start_index = (page - 1) * page_size
-        end_index = page * page_size
-        return start_index, end_index
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        data = self.get_page(page, page_size)
-        next_page = page + 1 if self.index_range(page + 1, page_size)[0] < len(self.__dataset) else None
-        prev_page = page - 1 if page > 1 else None
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
-
-        return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
-
-# Test the Server class
-if __name__ == "__main__":
-    server = Server()
-
-    print(server.get_hyper(1, 2))
-    print("---")
-    print(server.get_hyper(2, 2))
-    print("---")
-    print(server.get_hyper(100, 3))
-    print("---")
-    print(server.get_hyper(3000, 100))
+        return page_info
