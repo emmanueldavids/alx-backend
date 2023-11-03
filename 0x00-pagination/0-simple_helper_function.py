405219f6a0f41af266e46bba4c1f424439ae8967
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:00:14 +0000

    added files

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
+"""
+Simple function to calculate start and end indexes
+"""
+
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple:
     """
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
+    @page refers: the specific page required
+    @page_size: how much a specific page is
+    ex: for the following dataset [x for x in range(21)]
+        if a single page is 7 elements, the start and end indexes
+        of the first page would be 0, 7
     """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
+    return ((page_size * page) - page_size, (page_size * page))
