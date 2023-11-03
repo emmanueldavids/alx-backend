e5bc3b34799210e71e21e2ab232b71d9096482c8
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:04:19 +0000

    added files

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
-""" Create a class BasicCache that inherits from BaseCaching and is a caching system: """
+""" Create a class BasicCache that inherits
+    from BaseCaching and is a caching system:
+"""
+
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
-    """ Create a class BasicCache that inherits from BaseCaching and is a caching system: """
+    """ Create a class BasicCache that inherits
+        from BaseCaching and is a caching system:
+    """
 
     def __init__(self):
         super().__init__()
         self.cache_data = {}
 
     def put(self, key, item):
-        """ This method must assign to the dictionary self.cache_data the item value for
-            the key key.
+        """ This method must assign to the dictionary self.cache_data
+            the item value for the key key.
         """
         # self.cache_data = {}
         if key is None or item is None:
